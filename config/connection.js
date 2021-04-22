0ae3f62406b29ad3ac881dafb201fd9fae5b056f
Megan Russell <megan.russell50@gmail.com>
2021-04-22 15:58:08 -0700

    connection.js adjustments

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 const Sequelize = require('sequelize');
+const mysql = require('mysql');
+const connection;
+
 require('dotenv').config();
+
 let sequelize;
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    connection = mysql.createConnection({
         host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
+        user: 'root',
+        password: 'password1!',
+        database: 'tech_blog',
     });
 }
-module.exports = sequelize;
+
+connection.connect();
+module.exports = connection;
