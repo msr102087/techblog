823bd78c8dbcc6c27c00439970079bc7b3535e27
Megan Russell <megan.russell50@gmail.com>
2021-05-04 10:34:08 -0700

    handlebar update

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,56 +1,56 @@
 
-// async function newFormHandler(event) {
-//     event.preventDefault();
+async function newFormHandler(event) {
+    event.preventDefault();
   
-//     const title = document.querySelector('input[name="post-title"]').value;
-//     const content = document.querySelector('input[name="content"]').value;
-//     const token = localStorage.getItem('token')
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="post-url"]').value;
+    const token = localStorage.getItem('token')
 
 
   
-//     const response = await fetch(`/api/posts`, {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         title,
-//         content
-//       }),
-//       headers: {
-//         'Content-Type': 'application/json',
-//         authorization: `bearer ${token}`
-//       }
-//     });
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `bearer ${token}`
+      }
+    });
   
-//     if (response.ok) {
-//       document.location.replace('/dashboard');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   };
-  
-document.querySelector('#new-post').addEventListener('submit', async function(event){event.preventDefault();
-  
-  const title = document.querySelector('input[name="post-title"]').value;
-  const content = document.querySelector('input[name="content"]').value;
-  const token = localStorage.getItem('token')
-
-
-
-  const response = await fetch(`/api/posts`, {
-    method: 'POST',
-    body: JSON.stringify({
-      title,
-      content
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `bearer ${token}`
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
-  });
+  };
+  
+// document.querySelector('#new-post').addEventListener('submit', async function(event){event.preventDefault();
+  
+//   const title = document.querySelector('input[name="post-title"]').value;
+//   const content = document.querySelector('input[name="content"]').value;
+//   const token = localStorage.getItem('token')
 
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert(response.statusText);
-  }
 
-});
+
+//   const response = await fetch(`/api/posts`, {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       title,
+//       content
+//     }),
+//     headers: {
+//       'Content-Type': 'application/json',
+//       authorization: `bearer ${token}`
+//     }
+//   });
+
+//   if (response.ok) {
+//     document.location.replace('/dashboard');
+//   } else {
+//     alert(response.statusText);
+//   }
+
+// });
